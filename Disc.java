90706ecd2de73d7d5b711433d3c0b364435f8067
w00zzz <w00zzz@proton.me>
2023-03-04 11:04:55 -0500

    Se termino el codigo del programa del discriminante

--- Disc.java
+++ Disc.java
@@ -1,14 +1,22 @@
-import java.utils.Scanner;
+import java.util.Scanner;
 public class Disc{
   public static void main(String[] args){
     //if disc > 0, tiene 2 soluciones; disc == 0 tiene una solucion; disc menor a 0 no tiene solucion
+    double a, b, c, disc;
     Scanner input = new Scanner(System.in);
     System.out.print("Inroduce el valor de a: ");
-    double a = input.nextDouble();
+    a = input.nextDouble();
     System.out.print("Inroduce el valor de b: ");
-    double b = input.nextDouble();
+    b = input.nextDouble();
     System.out.print("Inroduce el valor de c: ");
-    double c = input.nextDouble();
-    disc
+    c = input.nextDouble();
+    disc = Math.pow(b, 2)-4*a*c;
+    if (disc > 0){
+      System.out.println("Tiene mas de una solucion en el conjunto de numeros reales");
+    }else if (disc == 0){
+      System.out.println("Tiene una solucion en el conjunto de numeros reales");
+    }else {
+      System.out.println("No tiene solucion en el conjunto de los numeros reales");
+    }
   }
 }
