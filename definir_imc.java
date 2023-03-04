4ab1d0970a6f7e56d555a36160e9c615432d8fb4
w00zzz <w00zzz@proton.me>
2023-03-04 08:57:01 -0500

    Se crearon los primeros archivos java

---
+++ definir_imc.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class definir_imc{
+  public static void main(String[] args){
+    float peso, talla, imc;
+    System.out.print("Introduce tu peso: ");
+    Scanner input = new Scanner(System.in);
+    peso = input.nextFloat();
+    System.out.print("Introduce tu talla en metros: ");
+    talla = input.nextFloat();
+    imc = peso/(float)Math.pow(2, talla);
+    if (imc <= 18.5){
+      System.out.println("Estas muy delgado");
+    } else if (imc > 18.5 && imc <= 24.9){
+        System.out.println("Estas delgado");
+    } else if (imc > 25 && imc <=29.9){
+        System.out.println("Estas sobrepeso");
+    } else {
+        System.out.println("Estas obeso");
+    }
+  }
+}
